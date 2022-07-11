6b54ccfddcfa771f996ba434b32249afcfc2fc42
DmitryTakoy <efimdima@yandex.ru>
2022-07-11 20:22:35 +0300

    viewnurls added

--- yatube/posts/views.py
+++ yatube/posts/views.py
@@ -1,3 +1,10 @@
-from django.shortcuts import render
+from django.http import HttpResponse
 
 # Create your views here.
+def index(request):
+    return HttpResponse('Главная страница')
+
+
+def group_posts(request,slug):
+    return HttpResponse(f'{slug} КОЛИЧЕСТВО СТАТЕЙ В ИНТЕРНЕТЕ, ГДЕ В КАЧЕСТВЕ ПРОСТОГО ПРОЕКТА НА DJANGO ПРЕДЛАГАЕТСЯ СДЕЛАТЬ ВАШ БЛОГ, С ПОДРОБНЫМИ ИНСТР. НО ЗА БЕСПЛАТНО: 1 054 564 651 220 шт. ')
+
